cbaaea9ef08c4dcd61fe2665ec4fa63c842edc15
Atef Mechken <atef.mechken@youngminds.tn>
2019-10-28 16:05:56 +0100

    Header file initialized

--- holberton.h
+++ holberton.h
@@ -1,4 +1,32 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
+
+/*===============================*/
+
+/*Dynamic function selection:*/
+
+/**
+* struct typeToPrint - the struct.
+* @types: the format.
+* @printType: the according function.
+* Return: void
+*/
+typedef struct typeToPrint
+{
+char *types;
+int (*printType)(va_list);
+} typeToPrint;
+
+/*===============================*/
+
+/*Printing functions:*/
+int printChar(va_list);
+int printString(va_list);
+int printPercent(va_list);
+
+/*===============================*/
+
+/*Main function:*/
 int _printf(const char *format, ...);
+
 #endif
