cb8095b320d5ef50f35d3705f775a4c693e3fc57
Atef Mechken <atef.mechken@youngminds.tn>
2019-10-28 16:14:22 +0100

    Adding the printing functions

---
+++ printingFuncs.c
@@ -0,0 +1,44 @@
+#include <stdarg.h>
+#include "holberton.h"
+
+/*Printing functions definition:*/
+
+/**
+* printChar - Print a character and increments obj.
+* @obj: the list of arguments.
+* Return: +1 character.
+*/
+int printChar(va_list obj)
+{
+_putchar(va_arg(obj, char));
+return (1);
+}
+
+/**
+* printString - Print a string and increments obj.
+* @obj: the list of arguments.
+* Return: + number of characters printed.
+*/
+int printString(va_list obj)
+{
+char *ch = va_arg(obj, char *);
+int i = 0;
+while (ch[i]) {
+_putchar(ch[i]);
+i++;
+}
+return (i - 1);
+}
+
+/**
+* printPercent - Prints a percent sign.
+* @obj: the list of arguments.
+* Return: +1 character.
+*/
+int printPercent(va_list obj)
+{
+_putchar('%');
+return (1);
+}
+
+/*===============================*/
