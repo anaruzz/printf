5c6ef74e2acac90e72ce623b62e4569c04a6d5ee
Atef Mechken <atef.mechken@youngminds.tn>
2019-10-28 17:22:33 +0100

    Updating the printf function

--- _printf.c
+++ _printf.c
@@ -1,5 +1,6 @@
 #include "holberton.h"
 #include <stdarg.h>
+#include <stdio.h>
 /**
 * _printf - produce output
 * @format: String
@@ -7,36 +8,46 @@
 */
 int _printf(const char *format, ...)
 {
-int i, j, n;
-va_list obj;
-typeToPrint arr[] = {
-  {"c", printChar},
-  {"s", printString},
-  {"%", printPercent},
-  {NULL, NULL},
-};
-if (format == NULL)
-return (-1);
 
-va_start(obj, format);
-i = 0;
-n = 0;
-while(format && format[i])
-{
-j = 0;
-while(arr[j].types)
-{
-if (format[i]) == '%')
-{
-if (arr[j].types == format[i + 1])
-{
-arr[j].printType(obj);
-n++;
-j++;
-}
-i++;
-}
-}
-va_end(obj);
-return (n);
+  int i, j, n;
+  va_list obj;
+  typeToPrint arr[] = {
+    {"c", printChar},
+    {"s", printString},
+    {"%", printPercent},
+    {NULL, NULL},
+  };
+
+  if (!format)
+  return (-1);
+
+  va_start(obj, format);
+
+  i = 0;
+  n = 0;
+
+  while(format && format[i])
+  {
+    j = 0;
+    if (format[i] == '%')
+    {
+      while(arr[j].types)
+      {
+        if (*(arr[j].types) == format[i + 1])
+        {
+          n += arr[j].printType(obj);
+        }
+        j++;
+      }
+      i++;
+    }
+    else
+    {
+      _putchar(format[i]);
+      n++;
+    }
+    i++;
+  }
+  va_end(obj);
+  return (n);
 }
