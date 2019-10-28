9edd343ae9637c6ee85075d790b828d3f068af37
Atef Mechken <atef.mechken@youngminds.tn>
2019-10-28 17:27:03 +0100

    Pushing fixes

--- _printf.c
+++ _printf.c
@@ -8,46 +8,37 @@
 */
 int _printf(const char *format, ...)
 {
-
-  int i, j, n;
-  va_list obj;
-  typeToPrint arr[] = {
-    {"c", printChar},
-    {"s", printString},
-    {"%", printPercent},
-    {NULL, NULL},
-  };
-
-  if (!format)
-  return (-1);
-
-  va_start(obj, format);
-
-  i = 0;
-  n = 0;
-
-  while(format && format[i])
-  {
-    j = 0;
-    if (format[i] == '%')
-    {
-      while(arr[j].types)
-      {
-        if (*(arr[j].types) == format[i + 1])
-        {
-          n += arr[j].printType(obj);
-        }
-        j++;
-      }
-      i++;
-    }
-    else
-    {
-      _putchar(format[i]);
-      n++;
-    }
-    i++;
-  }
-  va_end(obj);
-  return (n);
+int i = 0, j, n = 0;
+va_list obj;
+typeToPrint arr[] = {
+{"c", printChar},
+{"s", printString},
+{"%", printPercent},
+{NULL, NULL},
+};
+if (!format)
+return (-1);
+va_start(obj, format);
+while (format && format[i])
+{
+j = 0;
+if (format[i] == '%')
+{
+while (arr[j].types)
+{
+if (*(arr[j].types) == format[i + 1])
+n += arr[j].printType(obj);
+j++;
+}
+i++;
+}
+else
+{
+_putchar(format[i]);
+n++;
+}
+i++;
+}
+va_end(obj);
+return (n);
 }
