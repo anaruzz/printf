3ef6d5f38c4182f40280f49c4d1822b5efe39b81
anaruzz <laabidigh@gmail.com>
2020-02-03 04:01:03 +0100

    Betty fix Fail

--- _printf.c
+++ _printf.c
@@ -2,33 +2,6 @@
 #include <stdarg.h>
 #include <stdio.h>
 /**
- * spec - handle specifiers
- * @j : int
- * @format: format of output
- * @i: index of character in format
- * @obj: object to print
- * Return: int
-*/
-
-int spec(int j, const char *format, int i, va_list obj)
-{
-int n = 0;
-typeToPrint arr[] = {{"c", printChar},
-{"s", printString}, {"%", printPercent},
-{"i", printInteger}, {"d", printInteger}, {"b", printBinary},
-{"u", printUnsign}, {"o", printOctal}, {"x", printHexL},
-{"X", printHexU}, {"S", printString_S}, {"p", printPointer}, {NULL, NULL},};
-
-while (arr[j].types)
-{
-if (*(arr[j].types) == format[i + 1])
-n += arr[j].printType(obj);
-j++;
-}
-return (n);
-}
-
-/**
 * _printf - produce output
 * @format: String
 * Return: int
@@ -37,6 +10,10 @@ int _printf(const char *format, ...)
 {
 int i = 0, j, n = 0, v = 0;
 va_list obj;
+typeToPrint arr[] = {{"c", printChar}, {"s", printString}, {"%", printPercent},
+{"i", printInteger}, {"d", printInteger}, {"b", printBinary},
+{"u", printUnsign}, {"o", printOctal}, {"x", printHexL},
+{"X", printHexU}, {"S", printString_S}, {"p", printPointer}, {NULL, NULL},};
 if (format == NULL)
 return (-1);
 va_start(obj, format);
@@ -45,9 +22,15 @@ while (format && format[i])
 j = 0;
 if (format[i] == '%')
 {
-n = spec(j, format, i, obj);
-if (n > 0)
+while (arr[j].types)
+{
+if (*(arr[j].types) == format[i + 1])
+{
+n += arr[j].printType(obj);
 v = 1;
+}
+j++;
+}
 if (v == 0 && format[i + 1] != '\0')
 {
 _putchar(format[i]);
